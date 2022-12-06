ae22166bd93a2fbae60ccfb6ba46d729ba128d75
Sepehr <64617597+SepehrRamezani@users.noreply.github.com>
2022-12-06 00:41:47 -0500

    Stable code for calculating moment arm
    optimized parameters of wrapping put in opensim model and the moment  arm is calculated

--- Momentarm_Calculator.m
+++ Momentarm_Calculator.m
@@ -4,7 +4,7 @@ Basepath=Pardata{1};
 import org.opensim.modeling.*;
 
 % wrapping object optimization
-% put in opensim get moment arm 
+% put in opensim get moment arm
 subject=["p7"];
 Knee = ["K0","K30","K60","K90","K110"];
 Ankle = ["0","D10","P30"];
@@ -21,10 +21,35 @@ for K=1:length(Knee)
     for A=1:length(Ankle)
         fcoboname=append(Knee(K),"_",Ankle(A));
         Wrapping_param=Data.(fcoboname).WrappingPar;
-        curbody=model.getBodySet.get('tibia_l');
-        WrapObj=curbody.getWrapObjectSet().get('GasLat_at_shank_l');
-        set_radius()
-        set_translation (Vec3())
-        set_xyz_body_rotation (Vec3())
+        wrap_r=Wrapping_param(1);
+        wrap_y=-1*Wrapping_param(2);
+        wrap_x=-1*Wrapping_param(3);
+        Curbody=model.getBodySet.get('tibia_l');
+        
+        Wrapobj=Curbody.getWrapObjectSet().get('GasLat_at_shank_l');
+        CylinderWrapobj=WrapCylinder.safeDownCast(Wrapobj);
+        CylinderWrapobj.set_radius(wrap_r);
+        wrap_z=CylinderWrapobj.get_translation().get(2);
+        CylinderWrapobj.set_translation(Vec3(wrap_x,wrap_y,wrap_z));
+        model.initSystem();
+        kneeangle= double(erase(Knee(K),"K"));
+        if contains(Ankle(A),"P")
+            ankleangle=-1*double(erase(Ankle(A),"P"));
+        else
+            ankleangle=1*double(erase(Ankle(A),"D"));
+        end
+        
+        Musclename='gaslat_l';
+        kneecoord='knee_angle_l';
+        anklecoord='ankle_angle_l';
+        state = model.initSystem();
+        force = model.getForceSet().get(Musclename);
+        muscle = Millard2012EquilibriumMuscle.safeDownCast(force);
+        kneecoord=model.updCoordinateSet().get(kneecoord);
+        kneecoord.setValue(state, kneeangle);
+        model.updCoordinateSet().get(anklecoord).setValue(state, ankleangle);
+        model.realizePosition(state);
+        Momentarm = muscle.computeMomentArm(state, kneecoord);
+        Data.(fcoboname).Momentarm=Momentarm;
     end
 end
