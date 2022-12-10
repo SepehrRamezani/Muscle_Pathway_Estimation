9faa2e488640e8371a356dfc435402f68bcd7b13
Sepehr <64617597+SepehrRamezani@users.noreply.github.com>
2022-12-09 21:08:07 -0500

    function added to read c3d files and convert it to trc files

---
+++ c3dtotrc.m
@@ -0,0 +1,31 @@
+function c3dtotrc (filedata)
+Basepath=filedata.Basepath;
+load([Basepath '\US_raw.mat']);
+Knee=filedata.Knee;
+Ankle=filedata.Ankle;
+Trial=filedata.Trial;
+Subject=filedata.Subject;
+filedata.trial=[];
+for S=1:length(Subject) 
+    Trc_path=append(filedata.Basepath,'\Moca\',Subject,'\');
+for K=1:length(Knee)
+    for A=1:length(Ankle)
+        for T=1:length(Trial)
+            fname=append(Knee(K),"_",Ankle(A),"_L_",Trial(T));
+            markdatastruct = c3d_getdata(char(fullfile(Trc_path,append(fname,"_edited.c3d"))), 0);
+            Markerset=fieldnames(markdatastruct.marker_data.Markers);
+            Markerset=Markerset(~contains(Markerset,'C_'));
+            Markerset=markercheck(Markerset);
+            MarkerData=[markdatastruct.marker_data.Time];
+            for i = 1:length(Markerset)
+                MarkerData =[MarkerData markdatastruct.marker_data.Markers.(Markerset{i})];
+            end
+            
+            markdatastruct.marker_data.Info.Filename=erase(markdatastruct.marker_data.Info.Filename,'_edited');
+            generate_Marker_Trc(Markerset,MarkerData,markdatastruct.marker_data.Info);
+        end
+%%% Remove Extra Markers
+    end
+end
+end
+end
