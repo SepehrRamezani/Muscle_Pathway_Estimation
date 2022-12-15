8fbf2063a0028456e1dc297f72dd70a90e4fe6c2
Sepehr <64617597+SepehrRamezani@users.noreply.github.com>
2022-12-14 20:20:04 -0500

    Javalog is moved to main function
    Javalog is removed from the function and added to main function to prevent multi-call the logger

--- Main.m
+++ Main.m
@@ -1,17 +1,19 @@
 clear all
+% myLog = JavaLogSink();
+% Logger.addSink(myLog);
 Pardata=importdata('C:\MyCloud\GitHub\AddresseforMusclepathwayproject.txt');
 Data.Basepath=Pardata{1};
 Data.Subject=["p7"];
 Data.Knee = ["K0","K30","K60","K90","K110"];
-% Data.Knee = ["K0"];
+% Data.Knee = ["K30"];
 Data.Ankle = ["0","D10","P30"];
-% Data.Ankle = ["0"];
+% Data.Ankle = ["P30"];
 Data.Trial = ["1","2","3"];
 
-c3dtotrc(Data)
-US_Data_prepration(Data);
-Running_IK(Data);
-CombiningData(Data);
-MCP_Calculator(Data);
+% c3dtotrc(Data)
+% US_Data_prepration(Data);
+% Running_IK(Data);
+% CombiningData(Data);
+% MCP_Calculator(Data);
 WrapObject_Calculator(Data);
 FinalData=Momentarm_Calculator(Data);
