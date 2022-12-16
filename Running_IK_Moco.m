d9d1ca92d711a1d9b0374efd1cec10d5e3324f79
Sepehr <64617597+SepehrRamezani@users.noreply.github.com>
2022-12-15 21:59:51 -0500

    sample Moco code for running IK, very slow and not accurate

--- Running_IK_Moco.m
+++ Running_IK_Moco.m
@@ -1,65 +1,77 @@
+clear all
 Pardata=importdata('C:\MyCloud\GitHub\AddresseforMusclepathwayproject.txt');
-Data.Basepath=Pardata{1};
+Basepath=Pardata{1};
 import org.opensim.modeling.*;
+ myLog = JavaLogSink();
+Logger.addSink(myLog);
+SimMusclename= ["gaslat_l","gasmed_l"];
 Knee = ["K0"];
 Ankle = ["0"];
 Trial = ["1"];
 Subject=["p7"];
+Trc_path=append(Basepath,'\Moca\',Subject,'\');
+model=Model(append(Trc_path,Subject,"_raj_act1.osim"));
+load([Basepath '\US_raw.mat']);
+
 study = MocoStudy();
-study.setName('marker_tracking_10dof');
+study.setName('marker_tracking_US');
 problem = study.updProblem();
 problem.setModel(model);
-problem.setTimeBounds(0, 1.25);
+
 markerTrackingCost = MocoMarkerTrackingGoal();
 markerTrackingCost.setName('marker_tracking');
-model=Model(append(Trc_path,Subject,"_raj.osim"));
+
+markerWeights = SetMarkerWeights();
+
+% setup the marker weight 
+for mar = 0:1:model.getMarkerSet.getSize()-1
+    markername=model.getMarkerSet().get(mar).getName;
+    markerWeights.cloneAndAppend(MarkerWeight(char(markername), 1));
+end
+
+
+% problem.setStateInfo('joint/coord/value', [-10, 10]);
 
 
 for S=1:length(Subject)
-    Trc_path=append(Data.Basepath,'\Moca\',Subject,'\');
+    Trc_path=append(Basepath,'\Moca\',Subject,'\');
     for K=1:length(Knee)
         for A=1:length(Ankle)
             for T=1:length(Trial)
+
               fname=append(Knee(K),"_",Ankle(A),"_L_",Trial(T));  
-markersRef = MarkersReference(append(Trc_path,fname,"_Marker.trc"), ...
-    markerWeights);
+              us=Data.(fname).data;
+              problem.setTimeBounds(2, 5);
+markersRef = MarkersReference(append(Trc_path,fname,"_Marker.trc"), markerWeights);
 markerTrackingCost.setMarkersReference(markersRef);
 
-problem.addGoal(markerTrackingCost);
-controlCost = MocoControlGoal();
-controlCost.setWeight(0.001);
-problem.addGoal(controlCost);
+
             end
         end
     end
 end
 markerTrackingCost.setAllowUnusedReferences(true);
+problem.addGoal(markerTrackingCost);
+controlCost = MocoControlGoal();
+controlCost.setWeight(0.001);
+problem.addGoal(controlCost);
+
+
 solver = study.initCasADiSolver();
 solver.set_num_mesh_intervals(20);
-solver.set_optim_constraint_tolerance(1e-3);
+solver.set_optim_constraint_tolerance(1e-1);
 solver.set_optim_convergence_tolerance(1e-3);
 solver.setGuess('bounds');
-study.print('marker_tracking_10dof.omoco');
+solver.set_optim_max_iterations(4000);
+solver.set_implicit_auxiliary_derivatives_weight(0.00001)
+solver.set_parameters_require_initsystem(false);
+solver.resetProblem(problem);
+solver.set_verbosity(2);
+solver.set_optim_solver('ipopt');
+study.print(append(Trc_path,'marker_tracking_10dof.omoco'));
 solution = study.solve();
 solution.write('marker_tracking_10dof_solution.sto');
 if ~strcmp(getenv('OPENSIM_USE_VISUALIZER'), '0')
     study.visualize(solution);
 end
-function addCoordinateActuator(model, coordName, optimalForce)
-
-import org.opensim.modeling.*;
-
-coordSet = model.updCoordinateSet();
-
-actu = CoordinateActuator();
-actu.setName(['tau_' coordName]);
-actu.setCoordinate(coordSet.get(coordName));
-actu.setOptimalForce(optimalForce);
-% Set the min and max control defaults to automatically generate bounds for
-% each actuator in the problem.
-actu.setMinControl(-1);
-actu.setMaxControl(1);
-model.addComponent(actu);
-
-end
-
+Logger.removeSink(myLog);
