91961cf53aa24e02dbd42789784e42a668240c73
Sepehr <64617597+SepehrRamezani@users.noreply.github.com>
2022-12-29 02:56:35 -0500

    Checking markers task with existing marker

--- Running_IK.m
+++ Running_IK.m
@@ -9,14 +9,34 @@ Subject=Data.Subject;
 Data.trial=[];
 for S=1:length(Subject) 
     Trc_path=append(Data.Basepath,'\Moca\',Subject,'\');
+    model=Model(append(Trc_path,Subject,"_raj_modified.osim"));
+    model.getCoordinateSet().get('CLine_tx').set_locked(true);
+    model.getCoordinateSet().get('CLine_tz').set_locked(true);
+    model.initSystem();
 for K=1:length(Knee)
     for A=1:length(Ankle)
         for T=1:length(Trial)
             fname=append(Knee(K),"_",Ankle(A),"_L_",Trial(T));
             motion = Storage(append(Trc_path,fname,"_Marker.trc"));
+%             motiont=motion.exportToTable();
+            MarkerNameset=motion.getColumnLabels();
+            MarkerNames=[]
+            for j=1:3:MarkerNameset.getSize()-1
+                
+              MarkerNames=[MarkerNames erase(string(MarkerNameset.get(j)),"_x")];  
+            end
 %             us=Data.(fname).data;
-            model=Model(append(Trc_path,Subject,"_raj_act1.osim"));
+            
             ikTool=InverseKinematicsTool(append(Trc_path,'..\IK_Setup1.xml'));
+            Tskset=ikTool.getIKTaskSet();
+            for i=0:Tskset.getSize()-1
+                Task=ikTool.getIKTaskSet().get(i);
+                if ~sum(contains(MarkerNames,string(Task.getName())))
+                    Task.setApply(false)
+                    fprintf('%s does not have %s \n',fname,string(Task.getName()));
+                end
+                
+            end
             ikTool.setModel(model);
             ikTool.setMarkerDataFileName(append(Trc_path,fname,"_Marker.trc"));
             ikTool.setStartTime(motion.getFirstTime ());
