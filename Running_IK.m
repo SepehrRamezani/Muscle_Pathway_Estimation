6e3996a9dc8a8c8c9761033afd593eb658117e94
Sepehr <64617597+SepehrRamezani@users.noreply.github.com>
2023-01-03 02:59:34 -0500

    Checking the marker error of IK

--- Running_IK.m
+++ Running_IK.m
@@ -1,52 +1,71 @@
-function Running_IK(Data)
+function Running_IK(filedata)
 import org.opensim.modeling.*;
-Basepath=Data.Basepath;
+Basepath=filedata.Basepath;
 load([Basepath '\US_raw.mat']);
-Knee=Data.Knee;
-Ankle=Data.Ankle;
-Trial=Data.Trial;
-Subject=Data.Subject;
-Data.trial=[];
-for S=1:length(Subject) 
-    Trc_path=append(Data.Basepath,'\Moca\',Subject,'\');
-    model=Model(append(Trc_path,Subject,"_raj_modified.osim"));
+Knee=filedata.Knee;
+Ankle=filedata.Ankle;
+Trial=filedata.Trial;
+Subject=filedata.Subject;
+filedata.trial=[];
+for S=1:length(Subject)
+    Trc_path=append(filedata.Basepath,'\Moca\',Subject(S),'\');
+    model=Model(append(Trc_path,Subject(S),"_raj_modified.osim"));
     model.getCoordinateSet().get('CLine_tx').set_locked(true);
     model.getCoordinateSet().get('CLine_tz').set_locked(true);
     model.initSystem();
-for K=1:length(Knee)
-    for A=1:length(Ankle)
-        for T=1:length(Trial)
-            fname=append(Knee(K),"_",Ankle(A),"_L_",Trial(T));
-            motion = Storage(append(Trc_path,fname,"_Marker.trc"));
-%             motiont=motion.exportToTable();
-            MarkerNameset=motion.getColumnLabels();
-            MarkerNames=[]
-            for j=1:3:MarkerNameset.getSize()-1
-                
-              MarkerNames=[MarkerNames erase(string(MarkerNameset.get(j)),"_x")];  
-            end
-%             us=Data.(fname).data;
-            
-            ikTool=InverseKinematicsTool(append(Trc_path,'..\IK_Setup1.xml'));
-            Tskset=ikTool.getIKTaskSet();
-            for i=0:Tskset.getSize()-1
-                Task=ikTool.getIKTaskSet().get(i);
-                if ~sum(contains(MarkerNames,string(Task.getName())))
-                    Task.setApply(false)
-                    fprintf('%s does not have %s \n',fname,string(Task.getName()));
+    for K=1:length(Knee)
+        for A=1:length(Ankle)
+            for T=1:length(Trial)
+                fname=append(Knee(K),"_",Ankle(A),"_L_",Trial(T));
+                fullname=append(Subject(S),"_",fname);
+                markerdir=append(Trc_path,fname,"_Marker.trc");
+                if isfile(markerdir)
+                    motion = MarkerData(markerdir);
+                    %             motiont=motion.exportToTable();
+                    MarkerNameset=motion.getMarkerNames();
+                    MarkerNames=[];
+                    for j=0:MarkerNameset.getSize()-1
+                        MarkerNames=[MarkerNames string(MarkerNameset.get(j))];
+                    end
+                    %             us=Data.(fname).data;
+                    
+                    ikTool=InverseKinematicsTool(append(Trc_path,'..\IK_Setup1.xml'));
+                    Tskset=ikTool.getIKTaskSet();
+                    for i=0:Tskset.getSize()-1
+                        Task=ikTool.getIKTaskSet().get(i);
+                        if ~sum(contains(MarkerNames,string(Task.getName())))
+                            Task.setApply(false)
+                            fprintf('%s does not have %s \n',fullname,string(Task.getName()));
+                        end
+                        
+                    end
+                    ikTool.setName(fname)
+                    ikTool.setModel(model);
+                    ikTool.setResultsDir(Trc_path)
+                    %             ikTool.set_report_marker_locations(true);
+                    ikTool.setMarkerDataFileName(append(Trc_path,fname,"_Marker.trc"));
+                    ikTool.setStartTime(motion.getStartFrameTime());
+                    ikTool.setEndTime(motion.getLastFrameTime());
+                    outdir=append(Trc_path,fname,"_IK.mot");
+                    ikTool.setOutputMotionFileName(outdir);
+                    ikTool.print(append(Trc_path,"..\IK_Setup.xml"));
+                    ikTool.run();
+                    fprintf('IK of %s is done\n',fullname);
+                    ik_marker_Error_file = strrep(outdir, 'IK.mot', 'ik_marker_errors.sto');
+                    ik_marker_Error=importdata(ik_marker_Error_file);
+                    marker_error_RMS=mean(ik_marker_Error.data(:,3));
+                    if marker_error_RMS >= 0.02
+                        fprintf(2,'avarage marker_error_RMS of %s is %3.4f \n',fullname,marker_error_RMS);
+                        BadIKData.name=append(Subject(S),'_',fname);
+                        BadIKData.data=marker_error_RMS;
+                    end
+                else
+                    fprintf('Warning: trc file of %s was not found \n',fullname);
                 end
-                
+                %             Data.(Subject(S)).(fname).MarkerError=marker_error_RMS;
             end
-            ikTool.setModel(model);
-            ikTool.setMarkerDataFileName(append(Trc_path,fname,"_Marker.trc"));
-            ikTool.setStartTime(motion.getFirstTime ());
-            ikTool.setEndTime(motion.getLastTime());
-            ikTool.setOutputMotionFileName(append(Trc_path,fname,"_IK.mot"));     
-            ikTool.print(append(Trc_path,"..\IK_Setup.xml"));
-            ikTool.run();
-
         end
     end
 end
-end
+save([Basepath '\BadIK.mat'],'BadIKData');
 end
