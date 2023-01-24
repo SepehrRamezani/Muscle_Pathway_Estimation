a4b96ed153b1595d5b952b65859ec897631cb86b
Sepehr <64617597+SepehrRamezani@users.noreply.github.com>
2023-01-23 23:18:40 -0500

    rotation of wrapping obj fixed, model with optimized wrapping obj will be saved
    all default rotation of wrapping obj set to be zero except z.
    rotation around z was calculate base of LMML marker. higher rotation can cause wrong muscle pathway.

--- Momentarm_Calculator.m
+++ Momentarm_Calculator.m
@@ -1,19 +1,24 @@
  function [MCPData] = Momentarm_Calculator(filedata)
+ 
 Basepath=filedata.Basepath;
 import org.opensim.modeling.*;
 load([Basepath '\MCP_data.mat']);
+load([Basepath '\MomentArm_data.mat']);
 fcoboname=filedata.fcoboname;
 Model_path="";
 for S=1:length(fcoboname)
     trial_name=char(fcoboname(S));
     indxuderline=strfind(trial_name,'_');
     Subject=trial_name(1:indxuderline(1)-1);
-    Model_path_new=append(Basepath,'\Moca\',Subject,"\",Subject,"_raj.osim");
+    model_folder=fullfile(Basepath,'Moca',Subject);
+    Model_path_new=fullfile(model_folder,append(Subject,"_raj_modified.osim"));
     Knee=string(trial_name(indxuderline(1)+1:indxuderline(2)-1));
     Ankle=string(trial_name(indxuderline(2)+1:end));
+%     to not import model every time takes the loop  
     if ~contains(Model_path,Model_path_new)
         Model_path=Model_path_new;
         model=Model(Model_path);
+        [ss,ee]=mkdir(fullfile(model_folder,'WrOptmodel'));
     end
  
             Wrapping_param=MCPData.(fcoboname(S)).WrappingPar;
@@ -23,19 +28,35 @@ for S=1:length(fcoboname)
 %           Opensim x -> code -y
             wrap_y=-1*Wrapping_param(2);
             wrap_x=-1*Wrapping_param(3);
+            wrap_z=0;
+            state = model.initSystem();
             Curbody=model.getBodySet.get('tibia_l');
-            
+
+            LLMLMarker=model.getMarkerSet.get('LLML');
+            LLMLMarker.changeFramePreserveLocation(state,Curbody);
+            Dpoint=[LLMLMarker.get_location.get(0) LLMLMarker.get_location.get(1)];
+            Cpoint=[wrap_x wrap_y];
+            Vec=Dpoint-Cpoint;
+            wrap_rz=pi()+atan2(Vec(2),Vec(1));
+            if wrap_rz < .05
+                fprintf('Possible error in wraps angle of %s \n',fcoboname(S));
+            end
             Wrapobj=Curbody.getWrapObjectSet().get('GasLat_at_shank_l');
             CylinderWrapobj=WrapCylinder.safeDownCast(Wrapobj);
+            
             CylinderWrapobj.set_radius(wrap_r);
-            wrap_z=CylinderWrapobj.get_translation().get(2);
             CylinderWrapobj.set_translation(Vec3(wrap_x,wrap_y,wrap_z));
+            CylinderWrapobj.set_xyz_body_rotation(Vec3(0,0,wrap_rz));
+            CylinderWrapobj.set_quadrant('y')
+            CylinderWrapobj.set_length(0.1);
+            CylinderWrapobj.upd_Appearance().set_visible(true);
             model.initSystem();
-            kneeangle= double(erase(Knee,"K"));
+            kneeangle= deg2rad(double(erase(Knee,"K")));
+
             if contains(Ankle,"P")
-                ankleangle=-1*double(erase(Ankle,"P"));
+                ankleangle=-1*deg2rad(double(erase(Ankle,"P")));
             else
-                ankleangle=1*double(erase(Ankle,"D"));
+                ankleangle=1*deg2rad(double(erase(Ankle,"D")));
             end
             
             Musclename='gaslat_l';
@@ -44,13 +65,19 @@ for S=1:length(fcoboname)
             state = model.initSystem();
             force = model.getForceSet().get(Musclename);
             muscle = Millard2012EquilibriumMuscle.safeDownCast(force);
-            kneecoord=model.updCoordinateSet().get(kneecoord);
-            kneecoord.setValue(state, kneeangle);
-            model.updCoordinateSet().get(anklecoord).setValue(state, ankleangle);
+            Kneecoord=model.updCoordinateSet().get(kneecoord);
+            Kneecoord.setValue(state, kneeangle);
+            Anklecoord=model.updCoordinateSet().get(anklecoord);
+            Anklecoord.setValue(state, ankleangle);
             model.realizePosition(state);
-            Momentarm = muscle.computeMomentArm(state, kneecoord);
-            MCPData.(fcoboname(S)).Momentarm=Momentarm;
+            Momentarm = muscle.computeMomentArm(state, Kneecoord);
+            Kneecoord.setDefaultValue(kneeangle);
+            Anklecoord.setDefaultValue(ankleangle);
+            model.setName(append(fcoboname(S),"_raj_Wrp_Opt.osim"));
+            model.print(fullfile(model_folder,'WrOptmodel',append(fcoboname(S),"_raj_Wrp_Opt.osim")));
+            MomentarmData=MCPData;
+            MomentarmData.(fcoboname(S)).Momentarm=Momentarm;
     
 end
-save([Basepath '\MomentArm_data.mat'],'MCPData');
+save([Basepath '\MomentArm_data.mat'],'MomentarmData');
 end
